73d36e2c18fb86fdeae86c34edb9a7f00033aee6
Will Lillis <wlillis@umass.edu>
2024-05-26 14:39:24 -0400

    Create skeleton for separate parsing package

---
+++ asm-docs-parse/src/main.rs
@@ -0,0 +1,9 @@
+// TODO: Write a simple CLI tool to take in the project's XML files,
+// serialize them, and write them to a file accessible by the main
+// server file
+// Will probably want to move existing tests over to this crate, and then write
+// some new tests to make sure we can properly deserialize the converted docs
+// in the main server
+fn main() {
+    println!("Hello, world!");
+}
